f7537e4cf67241738ab422440e3f0d75c1fea3f2
Ilya Maksimov <iluxamax@yandex.ru>
2023-12-08 12:23:38 +0300

    Renamed

---
+++ Проверка кратности 7 и 23.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+
+{
+    static void Main()
+
+    {
+
+    Console.Write("Введте число а: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a % 7 == 0 && a %  23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+    }
+}
